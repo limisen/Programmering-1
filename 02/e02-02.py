804ae92a8e7a5248325ca4a22b51f35fe86c9be4
limisen <liam.cosford@edu.nykoping.se>
2020-09-09 17:40:58 +0200

    Did my best to finish some tasks, started work on kap 2 tasks.

---
+++ 02/e02-02.py
@@ -0,0 +1,7 @@
+#Skriv ett program som adderar två variablers heltal i en tredje och sedan skriver ut summan. Kontrollera sedan även vad som händer om den ena variabeln har ett heltal och den andra har ett flyttal.
+
+tal1 = range(10)
+
+tal2 = range(0,10)
+
+print(tal1 + tal2)
--- 03/03-04.py
+++ 03/03-04.py
@@ -2,7 +2,8 @@ ordPris = input("Ge mig ett Ordinarie-pris: ")
 ordPris = int(ordPris)
 
 rabatt = input("Ge mig en rabatt: ")
+int(rabatt)
 extrapris = ordPris * rabatt
-int(extrapris)
+float(extrapris)
 
 print(extrapris)
