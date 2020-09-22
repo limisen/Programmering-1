92965b88c37e54dcd2f01835e61f75a20eb130c2
limisen <liam.cosford@edu.nykoping.se>
2020-09-22 17:15:24 +0200

    finnished all befor task 8 then started on further assignments

---
+++ 03/e03-01.py
@@ -0,0 +1,3 @@
+firstname = input("Vad är ditt förnamn: ") 
+lastname = input("Vad är ditt efternamn: ")
+print("Hejsan ",firstname, lastname)
---
+++ e03-03.py
@@ -0,0 +1,4 @@
+tal1 = 52.12
+tal2 = 31.5
+summa = tal1 + tal2
+print("{}")
