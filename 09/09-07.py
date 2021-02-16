127b2af5f63bdfb3620d053384f37b221c773259
limisen <liam.cosford@edu.nykoping.se>
2021-02-16 10:39:12 +0100

    safe keeping

--- 09/09-07.py
+++ 09/09-07.py
@@ -12,5 +12,5 @@ areans_radie = radie(42.73)
 print('Kod saknas här')
 
 #Vet ej hur jag ska (fortsätta/vad jag ska göra)
-#Vilken enhet ska jag anvönda?
+#Vilken enhet ska jag använda?
 #vilka saker är funcktionen? och vilken är variabeln.
