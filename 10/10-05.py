eb91314872148529441f11cbfaa229cba55f0128
limisen <liam.cosford@edu.nykoping.se>
2021-03-16 15:22:15 +0100

    idk

--- 10/10-05.py
+++ 10/10-05.py
@@ -1,1 +1,12 @@
 from random import _sqrt
+
+
+Ange antal beräkningar till 10000000
+Ange innanför till 0
+Skriv ut Beräknar pi...
+Kör en loop för antalet beräkningar
+....Slumpa fram ett tal för x
+....Slumpa fram ett tal för y
+....Använd given kod i boken, if sqrt(xx + yy) < 1: Om det är sant, öka innanför med 1
+Räkna ut pi, pi = 4 * innanför/antal
+Skriv ut värdet med tre decimaler
---
+++ 10/10-06a.py
@@ -0,0 +1,15 @@
+from random import randrange
+
+def tärnings_kast():
+    """ Slumpar fram tärnings kast för att sedan skriva ut en frekvenstabell för de olika utfallen """
+    resultat = ['dummy', 'dummy', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    kast_för_tärning1 = [] 
+    kast_för_tärning2 = []
+
+    for i in range(10):
+        kast_för_tärning1.append(randrange(2,12))
+        kast_för_tärning2.append(randrange(2,12))
+        resultat.append(i,((kast_för_tärning1[i]+ kast_för_tärning2[i]))
+    
+
+print(tärnings_kast())
