65b441a6ba388f241af4d9c51f90d44b8890e6bf
limisen <liam.cosford@edu.nykoping.se>
2021-04-20 11:10:01 +0200

    All files should be on github now

---
+++ 16/16-e01.py
@@ -0,0 +1,11 @@
+pannkaka = {
+    'vetemjöl'  : '2 1/2 dl',
+    'salt'      : '1/2 tsk',
+    'mjölk'     : '6 dl',
+    'ägg'       : '3'
+}
+print(pannkaka.items())
+print("-------------------------")
+print(pannkaka.keys())
+print("-------------------------")
+print(pannkaka.values())
---
+++ 16/16-e02.py
@@ -0,0 +1,20 @@
+pannkaka = {
+    'vetemjöl': '2 1/2 dl',
+    'salt': '1/2 tsk',
+    'mjölk': '6 dl',
+    'ägg': '3'
+}
+
+for i in range(0, 5) {
+    for i in pannkaka {
+        print(pannkaka.values())
+    }
+    print("-------------------")
+    for i in pannkaka {
+        print(pannkaka.keys())
+    }
+    print("-------------------")
+    for i in pannkaka {
+        print(pannkaka.items())
+    }
+}
