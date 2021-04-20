a8c2f64a961545357922e2952a8ed87c848f0e00
limisen <liam.cosford@edu.nykoping.se>
2021-04-20 11:29:48 +0200

    just got to task 2, didn't finish it

--- 16/16-e02.py
+++ 16/16-e02.py
@@ -1,20 +1,11 @@
 pannkaka = {
-    'vetemjöl': '2 1/2 dl',
-    'salt': '1/2 tsk',
-    'mjölk': '6 dl',
-    'ägg': '3'
+    'vetemjöl'  : '2 1/2 dl',
+    'salt'      : '1/2 tsk',
+    'mjölk'     : '6 dl',
+    'ägg'       : '3'
 }
-
-for i in range(0, 5) {
-    for i in pannkaka {
-        print(pannkaka.values())
-    }
-    print("-------------------")
-    for i in pannkaka {
-        print(pannkaka.keys())
-    }
-    print("-------------------")
-    for i in pannkaka {
-        print(pannkaka.items())
-    }
-}
+print(str(pannkaka.values()))
+print("-------------------")
+print(str(pannkaka.keys()))
+print("-------------------")
+print(str(pannkaka.items()))
